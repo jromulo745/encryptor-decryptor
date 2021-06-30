3c611d33bb949fdff0e9dd31a4404accc48e2954
Joshua Jefson Romulo <jromulo745@gmail.com>
2021-06-30 16:33:34 -0700

    coded decryption function and completed main section of the program.

--- encryptor_decryptor.py
+++ encryptor_decryptor.py
@@ -24,8 +24,41 @@ def encryptor(string):
     encrypted_string += string_mirror
     return encrypted_string
     
-    
 #---------------------------------------------------------------------------------------
+
+def decryptor(string):
+    decrypted_string = ""
+    string_list = []
+    counter_1 = 0
+    counter_2 = -1
+    string_mirror = string
+    
+    for x in range(len(string_mirror)):
+        string_list.append("")
+        
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    while (len(string_mirror) != 0):
+        if (len(string_mirror) != 1):
+            string_list[counter_1] = string_mirror[0]
+            string_list[counter_2] = string_mirror[1]
+            string_mirror = string_mirror[2:]
+            counter_1 += 1
+            counter_2 -= 1
+        else:
+            string_list[int(len(string) / 2)] = string_mirror
+            string_mirror = ""
+            
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    for letter in string_list:
+        decrypted_string += letter
+        
+    return decrypted_string
+
+#---------------------------------------------------------------------------------------
+
+os.system("clear")
 
 # main section
 program_runs = True
@@ -33,8 +66,9 @@ program_runs = True
 while (program_runs == True):
     print()
     print("\t1 - Encryptor")
-    print("\t2 - Quit")
-    user_input = int(input("\n\tSelect a choice (1 - 2): "))
+    print("\t2 - Decryptor")
+    print("\t3 - Quit")
+    user_input = int(input("\n\tSelect a choice (1 - 3): "))
     os.system("clear")
     if (user_input == 1): # encrypt
         password = str(input("\n\tEnter a password to encrypt: "))
@@ -42,14 +76,16 @@ while (program_runs == True):
         print("\n\tHere is your encrypted password: " + string)
         wait_input = str(input("\n\tHit enter to continue..."))
         os.system("clear")
-    elif (user_input == 2): # quit
+    elif (user_input == 2):
+        password = str(input("\n\tEnter encrypted password: "))
+        string = decryptor(password)
+        os.system("clear")
+        print("\n\tHere is your decrypted password: " + string)
+        wait_input = str(input("\n\tHit enter to continue..."))
+        os.system("clear")
+    elif (user_input == 3): # quit
         program_runs = False
     else:
         print(" --------------- ")
         print("| Invalid Input |")
         print(" ---------------")
-    
-
-    
-
-
